ae4ea5f0c5f2c145c8707e87f791e6f212ea8628
Cyril <cy.jimenez@laposte.net>
2013-09-11 15:45:01 +0200

    changé nom classe trierTab

--- tp1/trierTab/TrierTab.java
+++ tp1/trierTab/TrierTab.java
@@ -1,29 +1,32 @@
-public class TrierTab {
-
-	public static void main(String[] args) {
-		
-		int[] tab = {15, 45, 0, 524, 82, 1, 256};
-		int tmp = 0;
-		
-		for(int i = 0; i < tab.length; i++) {
-			for(int j = i; j < tab.length; j++) {
-				if(tab[j] < tab[i]) {
-					tmp = tab[i];
-					tab[i] = tab[j];
-					tab[j] = tmp;
-				}
-			}		
-		}
-
-		System.out.println("Notes de la plus petite à la plus grande: ");
-
-		for (int k = 0; k < tab.length ; k++) {
-			System.out.print(tab[k] + " ");
-
-			if(k == tab.length){
-				System.out.println("\n");
-			}
-		}
-
-	}
+public class Tableau {
+ 
+    public static void main(String[] args) {
+        int[] tab = {15, 45, 0, 524, 82, 1, 256};
+        trierInt(tab);
+        printInt(tab);
+    }
+ 
+    public static int[] trierInt(int[] tab) {
+         
+        int tmp = 0;
+         
+        for(int i = 0; i < tab.length; i++) {
+            for(int j = i; j < tab.length; j++) {
+                if(tab[j] < tab[i]) {
+                    tmp = tab[i];
+                    tab[i] = tab[j];
+                    tab[j] = tmp;
+                }
+            }      
+        }
+ 
+        return tab;
+    }
+ 
+    public static void afficherInt(int[] tab) {
+        for (int k = 0; k < tab.length ; k++) {
+            System.out.println(tab[k]);
+        }
+         
+    }
 }
